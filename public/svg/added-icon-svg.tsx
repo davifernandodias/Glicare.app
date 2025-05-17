70ac49967cbecfb0c8f0ebc04028087279f4c3bc
Davi Dias <davifernandosousadias18@gmail.com>
2025-05-16 22:20:54 -0300

    refactor: refactoring of svg file, transforming it into a component to follow good practices and creating a specific directory for svg and png

---
+++ public/svg/added-icon-svg.tsx
@@ -0,0 +1,12 @@
+import { ISVG } from "../../src/interfaces/svg-interface"
+
+export const AddedIconSvg = ({ width, height, className }: ISVG) => {
+  return (
+    <svg width={width} height={height} className={className} viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="18" cy="18" r="18" fill="#1365A0" />
+      <line x1="8" y1="18" x2="28" y2="18" stroke="white" stroke-width="2" />
+      <line x1="18" y1="28" x2="18" y2="8" stroke="white" stroke-width="2" />
+    </svg>
+
+  )
+}
---
+++ public/svg/background-glicare-svg.tsx
@@ -0,0 +1,15 @@
+import { ISVG } from "../../src/interfaces/svg-interface"
+
+export const BackGroundGlicareSvg = ({ width, height, className }: ISVG) => {
+  return (
+    <svg width={width} height={height} className={className} viewBox="0 0 1621 1293" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+      <rect width={width} height={height}fill="url(#pattern0_14_106)" />
+      <defs>
+        <pattern id="pattern0_14_106" patternContentUnits="objectBoundingBox" width="1" height="1">
+          <use xmlnsXlink="#image0_14_106" transform="scale(0.000616903 0.000773395)" />
+        </pattern>
+      </defs>
+    </svg>
+
+  )
+}
