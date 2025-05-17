d6f81d33b7a31a52384417aa15690561b2a7fbd2
Davi Dias <davifernandosousadias18@gmail.com>
2025-05-16 22:07:09 -0300

    feat: added alias in export be import in components"alias added in export for import within components

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
+import path from 'path';
+
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
+      '@interface': path.resolve(__dirname, './src/interfaces'),
+      '@page': path.resolve(__dirname, './src/pages'),
+      '@svg': path.resolve(__dirname, './public/svg'),
+    }
+  }
 });
